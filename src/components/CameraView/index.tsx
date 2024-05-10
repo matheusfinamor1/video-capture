914409025f50a407e924950b491e39ba96fa8500
matheusfinamor1 <matheusfinamor1@gmail.com>
2024-05-10 15:21:51 -0300

    define components

---
+++ src/components/CameraView/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+
+import { CameraViewProps } from './props'
+import { styles } from './styles'
+
+export default function CameraView(){
+    return(
+        <Text>Camera View Works</Text>
+    )
+}
---
+++ src/components/VideoPlayer/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+
+import { VideoPlayerProps } from './props'
+import { styles } from './styles'
+
+export default function VideoPlayer(){
+    return(
+        <Text>Video Player Works</Text>
+    )
+}

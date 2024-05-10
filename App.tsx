2f51e38480df167aab832ac995b72caa82fabd9a
matheusfinamor1 <matheusfinamor1@gmail.com>
2024-05-10 13:56:32 -0300

    base project with imports

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+
+import { useEffect, useState, useRef } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+
+import { Camera } from 'expo-camera'
+import { Video } from 'expo-av'
+import { shareAsync } from 'expo-sharing'
+import * as MediaLibrary from 'expo-media-library'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
---
+++ docs/docs.md
@@ -0,0 +1,17 @@
+# camera
+https://docs.expo.dev/versions/latest/sdk/camera/#camerapictureoptions
+npx expo install expo-camera
+
+# video e audio
+https://docs.expo.dev/versions/latest/sdk/av/
+npx expo install expo-av
+
+# albuns
+https://docs.expo.dev/versions/latest/sdk/media-library/
+npx expo install expo-media-library
+
+# sharing
+https://docs.expo.dev/versions/latest/sdk/sharing/
+npx expo install expo-sharing
+
+______________________________________________________________________________
